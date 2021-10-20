e01475ea9f0e327300f8ba07b17894abee03db12
Iban Lopez <ibanlopezd@gmail.com>
2021-10-20 20:37:18 +0200

    ADDED: Order controller for CRUD operations

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -2,6 +2,7 @@ const Order = require('../models/Order.model')
 
 exports.findAll = async (req, res) => {
     try {
+        // Procurar todos os pedidos
         const orders = await Order.findAll()
         if (!orders) {
           return  res.send({message: 'Não foram achados pedidos'})
@@ -16,54 +17,60 @@ exports.findAll = async (req, res) => {
 exports.findOne = async (req, res) => {
     try {
         const id = req.params.id
+        // Procurar pedido com Id do pedido
         const order = await Order.findByPk(id)
         if (!order) {
            return res.send({ message: `Não foi achado nenhum pedido com ID ${id}` })
         } else {
-           return res.send({ message: order })
+           return res.send(order)
         }
     } catch (err) {
       return res.send({message: `Algo nao foi certo carregando o pedido com ID: ${id}`})
     }
 }
 
-exports.addUser = async (req, res) => {
+exports.addOrder = async (req, res) => {
     try {
-        const { email, password } = req.body
-        if (!email) {
-           return res.send({ message: 'Email is missing' })
-        }
-        if (!password) {
-            return res.send({ message: 'Password is missing' })
-        }
-
-        if (email && password) {
-            // Check if user is already in the database
-            const myUser = await User.findOne({where: { email: email }})
-            if (myUser) {
-                return res.send({ message: `Usuário com E-Mail ${email} já está cadastrado`})
-            } else {
-                // Check if E-Mail provided has a valid format
-                const validEmail = validateMail(email)
-                if (validEmail) {
-                    // Encrypt User password before saving
-                    const hash = bcrypt.hashSync(password, 10)
-                    // Save User into DB
-                    const saveUser = await User.create({
-                       email: email,
-                       senha: hash
-                    })
-                    // Inform User was created
-                    if (saveUser) {
-                        return res.send({ message: `Usuário com E-Mail: ${email} foi criado` })
-                    }
-                } else {
-                    return res.send({ message: `${email} não é um E-Mail válido!` })
+        const { customer, waiter, table, products, total } = req.body
+            // Salvar novo pedido no banco de dados
+            if (customer, waiter, table, products) {
+                const saveOrder = await Order.create({
+                    customer: customer,
+                    waiter: waiter, 
+                    table: table,
+                    products: products,
+                    total: total,
+                    completed: false
+                })
+                // Informar que o pedido foi criado
+                if (saveOrder) {
+                    return res.send({ message: `Pedido na Mesa ${table} para ${cliente} foi criado || Produtos totais: ${products.length} || Total: R$ ${total}` })
                 }
+            } else {
+                return res.send(`Algo nao foi certo criando o pedido`)
             }
-        } 
-
     } catch (err) {
        return res.send({message: err.message})
+    }
+}
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const id = req.params.id
+        // Atualizar pedido dependendo da instruçao
+        switch (req.body.type) {
+            case 'ADD_PRODUCT':
+                return res.send(`Produto foi adicionado para pedido ${id}`)
+            case 'DELETE_PRODUCT':
+                return res.send(`Produto foi excluido para pedido ${id}`)
+            case 'PAYMENT_DONE':
+                return res.send(`Pagamento feito para pedido ${id}`)
+            case 'COMPLETED':
+                return res.send(`Pedido completo || Mesa Libre`)
+            default:
+                return res.send(`Algo nao foi certo atualizando o pedido ${id}`)
+        }
+    } catch (err) {
+        return res.send({message: err.message})
     }
 }
