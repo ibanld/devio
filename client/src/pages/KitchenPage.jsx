4c31a8a37902145192fb94b50b094a7a0cd463cb
Iban Lopez <ibanlopezd@gmail.com>
2021-10-24 01:26:12 +0200

    ADDED: comments on Kitchen page // Kitchen page built

--- client/src/pages/KitchenPage.jsx
+++ client/src/pages/KitchenPage.jsx
@@ -5,27 +5,33 @@ import { useOrders } from '../context/ordersContext'
 import { Container, Header, Divider, Button } from 'semantic-ui-react'
 
 export default function KitchenPage() {
+    // State to set pending orders individually
     const [products, setProducts] = useState([])
+
+    // Get orders from global state Context (redux alikes)
     const { orders } = useOrders()
 
     useEffect( ()=> {
         if (orders.length > 0) {
             const getProducts = []
+            // Map though all orders and add products to array by mapping on products for each order
             orders.map( order => order.products.map( product => getProducts.push({orderId: order._id, customer: order.customer, table: order.table, ...product})))
             if (getProducts.length > 0) {
+                // Filter all pending products
                 const pending = getProducts.filter( product => !product.ready)
                 setProducts(pending)
             }
         }
     }, [orders])
-
+// Function to handle products when are ready
     const handleReady = async (product) => {
         const {orderId, _id} = product
         try {
+            // PUT into orders endpoint with ID and action type to set the product as READY
             const orderComplete = await API.put(`/orders/${orderId}`, {type: 'PRODUCT_READY', productId: _id})
             if (orderComplete) {
+                // If success the socket will update the orders state in real time
                 requestUpdate()
-
             }
         } catch (err) {
             console.error(err)
