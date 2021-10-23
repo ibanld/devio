ad0da0ea4ae173ab6708f8ee814f1a00ee5e5c6b
Iban Lopez <ibanlopezd@gmail.com>
2021-10-23 19:13:07 +0200

    ADDED: Delete orders

--- client/src/components/waiterPage/AddInfo.jsx
+++ client/src/components/waiterPage/AddInfo.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import API from '../../utils/axiosUrl'
 import requestUpdate from '../../utils/socketUpdate'
-import { Form } from 'semantic-ui-react'
+import { Form, Divider, Button } from 'semantic-ui-react'
 
 export default function AddInfo({ order }) {
     const [form, setForm] = useState({
         customer: order.customer,
-        comment: ''
+        comment: order.comment
     })
 
     const handleChange = e => {
@@ -22,7 +22,17 @@ export default function AddInfo({ order }) {
             const updateOrder = await API.put(`/orders/${order._id}`, {type: 'UPDATE_INFO', data: form})
             if (updateOrder) {
                 requestUpdate()
-                console.log(updateOrder.data)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const handleDelete = async id => {
+        try {
+            const delItem = await API.delete(`/orders/${id}`)
+            if (delItem) {
+                console.log(delItem.data.message)
             }
         } catch (err) {
             console.error(err)
@@ -30,6 +40,7 @@ export default function AddInfo({ order }) {
     }
 
     return (
+        <>
         <Form onSubmit={e => handleSubmit(e) }>
             <Form.Group widths="equal">
                 <Form.Input 
@@ -57,5 +68,15 @@ export default function AddInfo({ order }) {
                 content="Atualizar Informaçao"
             />
         </Form>
+        <Divider />
+        <Button 
+            type="button"
+            icon="trash"
+            content="Excluir Pedido"
+            onClick={ ()=> handleDelete(order._id)}
+            negative
+            fluid
+        />
+    </>
     )
 }
--- client/src/components/waiterPage/OrderDetail.jsx
+++ client/src/components/waiterPage/OrderDetail.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import API from '../../utils/axiosUrl'
 import AddProduct from './AddProduct'
 import AddInfo from './AddInfo'
 import OrderList from './OrderList'
@@ -8,9 +9,25 @@ import { useOrders } from '../../context/ordersContext'
 
 export default function OrderDetail() {
     const [showInfo, setShowInfo] = useState('order')
+    const [myOrder, setMyOrder] = useState({})
+    const { order, orders } = useOrders()
     
-    const { order } = useOrders()
-    
+    const getMyOrder = async id => {
+        try {
+            const getOrder = await API.get(`/orders/${id}`)
+            if (getOrder) {
+                setMyOrder(getOrder.data)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect( ()=> {
+        getMyOrder(order._id)
+        console.log('re-rendered')
+    }, [orders, order])
+
     return (
         <>
         <Divider />
@@ -46,12 +63,12 @@ export default function OrderDetail() {
                 />
             </Button.Group>
             <Divider />
-            {order.table === null ? 'loading' :
+            {myOrder.table === null ? 'loading' :
                 <>
-                    {showInfo === 'info' && <AddInfo order={order} />}
-                    {showInfo === 'order'&& <OrderList order={order} />}
-                    {showInfo === 'add' && <AddProduct order={order} />}
-                    {showInfo === 'payment' && <OrderPayment order={order} /> }
+                    {showInfo === 'info' && <AddInfo order={myOrder} />}
+                    {showInfo === 'order'&& <OrderList order={myOrder} />}
+                    {showInfo === 'add' && <AddProduct order={myOrder} />}
+                    {showInfo === 'payment' && <OrderPayment order={myOrder} /> }
                 </>
             }
         </Container>
