dc2b3d6ee99ebb65314febcd980440856f08ebb0
Iban Lopez <ibanlopezd@gmail.com>
2021-10-24 01:38:07 +0200

    ADDED: Comments on main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -11,15 +11,20 @@ import { Container } from 'semantic-ui-react'
 import AdminPage from './AdminPage'
 
 export default function MainPage(){
+    // State to handle different pages (views)
     const [view, setView] = useState(null)
+    // State to open views Modal menu
     const [open, setOpen] = useState(false)
-    //Placeholder values
+    // Placeholder values for tables (production mode should handle this from database or server)
     const [tables, setTables] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13])
 
+// Orders and log-in status from context state provider
     const { orders, logged } = useOrders()
+// Order dispatcher for context state managment (redux alike) 
     const dispatchOrders = useDispatchOrders()
-    
+// Connection to socket live server host    
     const socket = io("http://localhost:5000")
+
 
     useEffect( () => {
         socket.on("orders", (arg) => { 
@@ -33,6 +38,7 @@ export default function MainPage(){
     }, [])
     
     useEffect(() => {   
+        // If user is logged-in, the context provider loads the pending orders for the loaded User
         if (logged) {
             dispatchOrders({
                 type: 'CURRENT_ORDERS'
